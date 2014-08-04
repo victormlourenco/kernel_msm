911af775d9a2f382517d287a16496586e4062295
savoca <adeddo27@gmail.com>
2014-08-04 01:14:02 +0000

    msm: mipi_lgit: Add lcd_notify wrapper

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -20,6 +20,9 @@
 #include <linux/string.h>
 #include <linux/gpio.h>
 #include <linux/syscore_ops.h>
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+#include <linux/lcd_notify.h>
+#endif
 
 #include "msm_fb.h"
 #include "mipi_dsi.h"
@@ -71,6 +74,9 @@ static int mipi_lgit_lcd_on(struct platform_device *pdev)
 	struct msm_fb_data_type *mfd;
 	int ret = 0;
 
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+	lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
+#endif
 	pr_info("%s started\n", __func__);
 
 	mfd = platform_get_drvdata(pdev);
@@ -119,6 +125,9 @@ static int mipi_lgit_lcd_on(struct platform_device *pdev)
 		return ret;
 	}
 
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+	lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
+#endif
 	pr_info("%s finished\n", __func__);
 	return 0;
 }
@@ -128,6 +137,9 @@ static int mipi_lgit_lcd_off(struct platform_device *pdev)
 	struct msm_fb_data_type *mfd;
 	int ret = 0;
 
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+	lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
+#endif
 	pr_info("%s started\n", __func__);
 
 	if (mipi_lgit_pdata->bl_pwm_disable)
@@ -167,6 +179,9 @@ static int mipi_lgit_lcd_off(struct platform_device *pdev)
 		return ret;
 	}
 
+#ifdef CONFIG_FB_MSM_LCD_NOTIFY
+	lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
+#endif
 	pr_info("%s finished\n", __func__);
 	return 0;
 }
